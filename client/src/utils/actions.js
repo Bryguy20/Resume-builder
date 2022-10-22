87872b723175ed1870cf07bf7d8b926a59bf7e85
Bryguy <bryansegarra044@gmail.com>
2022-10-22 11:29:59 -0400

    the packages needed

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,1 +1,7 @@
-export const SAMPLE_ACTION = "SAMPLE_ACTION";
+export const ADD_Education = 'ADD_Education';
+
+export const Add_About = 'ADD_Education';
+
+export const ADD_Personal = 'ADD_Personal';
+
+export const ADD_Project = 'ADD_Project';
