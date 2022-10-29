7023781883c7d90e92ea3fa7818163a33d23e3e5
President <aaron.m.wilson@uconn.edu>
2022-10-29 05:52:01 -0400

    some styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,45 @@
-import { Navbar } from "react-bootstrap";
+import React from "react";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
+function Nav() {
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      );
+    }
+  }
 
-const NavBar = () => {
   return (
-    <nav style={{background: "pink", height: "100px"}}>
-      <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-          <h1 href="/" style={{textAlign: "center", alignItems: "center"}}>CV Generator App</h1>
-        </Navbar>
-      </header>
-    </nav>
-  );
-};
+  <body>
+    <header className="flex-row px-1" style={{background: "pink", height: "100px", textAlign: "center"}}>
+      <h1 style={{textAlign: "center"}}>Resume Generator</h1>
+      <br></br>
+      <h2 >Please create and account so we can start!</h2>
+    </header>
+    <nav style={{textAlign: "center", marginTop: "80px"}}>{showNavigation()}</nav> 
+  </body>
+  
 
-export default NavBar;
+  );
+}
+
+export default Nav;
---
+++ client/src/pages/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            Any questions please contact us at lord hve mercy
+        </footer>
+    )
+}
+
+export default Footer;
